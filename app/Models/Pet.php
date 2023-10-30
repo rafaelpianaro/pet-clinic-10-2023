6077e7476ccbf21576f5e3abd815007cf701a79d
Rafael Pianaro <rafaelpianaro@gmail.com>
2023-10-29 21:25:48 -0300

    create project with phpstan and add migrations

---
+++ app/Models/Pet.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Pet extends Model
+{
+    use HasFactory;
+
+    public function owner(): BelongsTo
+    {
+        return $this->belongsTo(Owner::class);
+    }
+
+    public function appointments(): HasMany
+    {
+        return $this->hasMany(Appointment::class);
+        // return $this->hasMany(Appointment::class, 'pet_id', 'id');
+    }
+}
